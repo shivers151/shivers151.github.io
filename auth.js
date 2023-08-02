b4fbf6b673794e8ee60a1497f23e85012108883a
Anthony Shivers <anthony.shivers@reliabilitydata.net>
2023-08-02 16:44:42 -0400

    home page

--- auth.js
+++ auth.js
@@ -12,7 +12,7 @@ const uiConfig = {
     },
   },
   signInFlow: "popup",
-  signInSuccessUrl: "signedIn",
+  signInSuccessUrl: "homepage.html",
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
