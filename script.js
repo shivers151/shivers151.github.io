e06acee0c88ba4e1606ce295797e463a9582ad70
Anthony Shivers <anthony.shivers@reliabilitydata.net>
2023-12-15 10:19:36 -0500

    dom stuff

--- script.js
+++ script.js
@@ -101,45 +101,49 @@ document.getElementById("exerciseForm").addEventListener("submit", function(even
 });
 
 
-const dropArea = document.getElementById('dropArea');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const dropArea = document.getElementById('dropArea');
 
-dropArea.addEventListener('dragover', (event) => {
+  dropArea.addEventListener('dragover', (event) => {
     event.stopPropagation();
     event.preventDefault();
     event.dataTransfer.dropEffect = 'copy';
-});
+  });
 
-dropArea.addEventListener('drop', (event) => {
+  dropArea.addEventListener('drop', (event) => {
     event.stopPropagation();
     event.preventDefault();
     const files = event.dataTransfer.files;
     if (files && files[0]) {
-        const file = files[0];
-        readSpreadsheet(file);
+      const file = files[0];
+      readSpreadsheet(file);
     }
+  });
 });
 
+
+
 function readSpreadsheet(file) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        const data = new Uint8Array(e.target.result);
-        const workbook = XLSX.read(data, { type: 'array' });
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: 'array' });
 
-        // Assuming data is in the first sheet
-        const firstSheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[firstSheetName];
+    // Assuming data is in the first sheet
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
 
-        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-        populateForm(jsonData);
-    };
-    reader.readAsArrayBuffer(file);
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+    populateForm(jsonData);
+  };
+  reader.readAsArrayBuffer(file);
 }
 
 function populateForm(data) {
-    // Assuming the data follows the structure: [name, sets, reps]
-    data.forEach(row => {
-        if (row.length >= 3) {
-            fillExerciseRow(row); // You need to define this function
-        }
-    });
+  // Assuming the data follows the structure: [name, sets, reps]
+  data.forEach(row => {
+    if (row.length >= 3) {
+      fillExerciseRow(row); // You need to define this function
+    }
+  });
 }
