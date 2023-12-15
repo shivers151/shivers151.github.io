ddc9459d663148818fb8baa2520eba6ead15dd96
Anthony Shivers <anthony.shivers@reliabilitydata.net>
2023-12-15 10:16:42 -0500

    allow spread sheet access

--- addSession.js
+++ addSession.js
@@ -11,7 +11,7 @@ const inputs = [{
 
 
 const rerenderPage = () =>{
-  
+
 };
 
 
@@ -35,3 +35,56 @@ const submitSession = () =>{
 };
 
 appendExercise();
+
+//const dropArea = document.getElementById('dropArea');
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const dropArea = document.getElementById('dropArea');
+  console.log("run")
+  dropArea.addEventListener('dragover', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+    console.log("DRAG OVER")
+  });
+
+
+  dropArea.addEventListener('drop', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    if (files && files[0]) {
+      const file = files[0];
+      readSpreadsheet(file);
+    }
+    console.log("DROP");
+  });
+
+});
+
+function readSpreadsheet(file) {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: 'array' });
+
+    // Assuming data is in the first sheet
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+    populateForm(jsonData);
+  };
+  reader.readAsArrayBuffer(file);
+}
+
+function populateForm(data) {
+  // Assuming the data follows the structure: [name, sets, reps]
+  data.forEach(row => {
+    if (row.length >= 3) {
+      addExerciseRow(row[0], row[1], row[2]); // You need to define this function
+    }
+  });
+}
